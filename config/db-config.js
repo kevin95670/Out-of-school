d90780d3098f3c8477f4783b155ba7623b142588
kevin Leonard <kevinleonard.info@gmail.com>
2020-06-10 22:14:25 +0200

    add scalingo connect mongoose

--- config/db-config.js
+++ config/db-config.js
@@ -19,7 +19,7 @@ var mongoose = require('mongoose');
 	.then(() => console.log("Successfully connect to MongoDB."))
 	.catch(err => console.error("Connection error", err));*/
 
-mongoose.connect('mongodb://out-of-school-9073:mNOmTxztjFeGnwYI3V5P@f861a787-6465-4fb5-9aea-e3eb2f408e35.out-of-school-9073.mongo.dbs.scalingo.com:30707/blog', 
+mongoose.connect('mongodb://out-of-school-9073:mNOmTxztjFeGnwYI3V5P@f861a787-6465-4fb5-9aea-e3eb2f408e35.out-of-school-9073.mongo.dbs.scalingo.com:30707/out-of-school-9073?replicaSet=out-of-school-9073-rs0&ssl=true', 
 	{ 
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
