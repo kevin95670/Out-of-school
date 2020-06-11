11cbed93438c60e72d15a1bd5df9f8baaefd4de7
kevin Leonard <kevinleonard.info@gmail.com>
2020-06-11 10:45:13 +0200

    change config for production

--- config/db-config.js
+++ config/db-config.js
@@ -9,17 +9,8 @@ Import
 var mongoose = require('mongoose');
 
 //Db connection
-/*mongoose.connect('mongodb://localhost/blog', 
-	{ 
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false
- 	})
-	.then(() => console.log("Successfully connect to MongoDB."))
-	.catch(err => console.error("Connection error", err));*/
-
-mongoose.connect('mongodb://out-of-school-9073:mNOmTxztjFeGnwYI3V5P@f861a787-6465-4fb5-9aea-e3eb2f408e35.out-of-school-9073.mongo.dbs.scalingo.com:30707/out-of-school-9073?replicaSet=out-of-school-9073-rs0&ssl=true', 
+if(process.env.NODE_ENV === 'production') {
+    mongoose.connect('mongodb://out-of-school-9073:mNOmTxztjFeGnwYI3V5P@f861a787-6465-4fb5-9aea-e3eb2f408e35.out-of-school-9073.mongo.dbs.scalingo.com:30707/out-of-school-9073?replicaSet=out-of-school-9073-rs0&ssl=true', 
 	{ 
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
@@ -28,3 +19,14 @@ mongoose.connect('mongodb://out-of-school-9073:mNOmTxztjFeGnwYI3V5P@f861a787-646
  	})
 	.then(() => console.log("Successfully connect to MongoDB."))
 	.catch(err => console.error("Connection error", err));
+} else {
+mongoose.connect('mongodb://localhost/blog', 
+	{ 
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+ 	})
+	.then(() => console.log("Successfully connect to MongoDB."))
+	.catch(err => console.error("Connection error", err));
+}
